01811344d6025fcc52e42ba0e12df6eb58f554b2
guohanlin <guohanlin@xyz.cn>
2020-07-10 17:37:47 +0800

    [新增功能](1.0.8): 新增修改版本历史选择面板

--- src/com/rmondjone/commit/CommitPannel.java
+++ src/com/rmondjone/commit/CommitPannel.java
@@ -1,8 +1,18 @@
 package com.rmondjone.commit;
 
+import com.intellij.icons.AllIcons;
 import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.popup.JBPopup;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
 
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
@@ -32,6 +42,18 @@ public class CommitPannel {
      * 弹框主面板
      */
     private JPanel mainPanel;
+    /**
+     * 提交版本历史按钮
+     */
+    private JButton mVersionHistoryButton;
+    /**
+     * 版本历史记录Pop
+     */
+    private JBPopup popup;
+    /**
+     * 版本历史记录
+     */
+    private List<String> versionFieldList = new ArrayList<>(2);
 
     public CommitPannel(Project project) {
         //设置提交类型默认值
@@ -39,19 +61,67 @@ public class CommitPannel {
             mTypeComboBox.addItem(type);
         }
         //设置提交版本默认值
-        String versionField = PropertiesComponent.getInstance().getValue("VersionField");
-        if (!"".equals(versionField) && versionField != null) {
-            mVersionField.setText(versionField);
+        String lastVersion = PropertiesComponent.getInstance().getValue("VersionField");
+        mVersionField.setText(lastVersion);
+        String[] versionFields = PropertiesComponent.getInstance().getValues("VersionFields");
+        if (versionFields != null) {
+            versionFieldList = Arrays.asList(versionFields);
+            Collections.reverse(versionFieldList);
         }
+        //初始化提交版本历史按钮
+        mVersionHistoryButton.setIcon(AllIcons.Vcs.History);
+        mVersionHistoryButton.setText("");
+        mVersionHistoryButton.addActionListener(e -> {
+            popup = creatVersionHistoryChoosePop(versionFieldList);
+            Dimension dimension = popup.getContent().getPreferredSize();
+            popup.setMinimumSize(new Dimension(160, dimension.height));
+            popup.showUnderneathOf(mVersionHistoryButton);
+        });
+    }
+
+    /**
+     * 注释：创建版本历史选择pop悬浮窗
+     * 时间：2020/7/10 0010 11:47
+     * 作者：郭翰林
+     *
+     * @param list
+     * @return
+     */
+    public JBPopup creatVersionHistoryChoosePop(List<String> list) {
+        JBPopup popup = JBPopupFactory.getInstance().createPopupChooserBuilder(list).setItemChosenCallback(s -> {
+            mVersionField.setText(s);
+        }).createPopup();
+        return popup;
     }
 
     public JPanel getMainPanel() {
         return mainPanel;
     }
 
+    /**
+     * 注释：保存提交历史版本
+     * 时间：2020/7/10 0010 15:43
+     * 作者：郭翰林
+     */
+    public void saveVersionFieldList() {
+        List<String> saveVersionFieldList = new ArrayList<>(versionFieldList);
+        Collections.reverse(saveVersionFieldList);
+        String version = mVersionField.getText().trim();
+        PropertiesComponent.getInstance().setValue("VersionField", version);
+        if (!saveVersionFieldList.contains(version)) {
+            saveVersionFieldList.add(version);
+            //只保留最近10次提交版本记录
+            if (saveVersionFieldList.size() > 10) {
+                List<String> subSaveVersionFieldList = saveVersionFieldList.subList(saveVersionFieldList.size() - 10, saveVersionFieldList.size());
+                PropertiesComponent.getInstance().setValues("VersionFields", subSaveVersionFieldList.toArray(new String[10]));
+            } else {
+                PropertiesComponent.getInstance().setValues("VersionFields", saveVersionFieldList.toArray(new String[versionFieldList.size() + 1]));
+            }
+        }
+    }
+
     public CommitMessage getCommitMessage() {
-        //记录版本值
-        PropertiesComponent.getInstance().setValue("VersionField", mVersionField.getText().trim());
+        saveVersionFieldList();
         return new CommitMessage(
                 (ChangeType) mTypeComboBox.getSelectedItem(),
                 mVersionField.getText().trim(),
